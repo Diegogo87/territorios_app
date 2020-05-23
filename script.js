4c1122b5e47029712082736c7f0d9a327e1ef8a8
Diegogo87 <49370289+Diegogo87@users.noreply.github.com>
2020-05-23 22:47:53 +0200

    Add files via upload
    He anadido los controles necesarios para la BD firebase asi como u  popup al hacer click con la opcion envia y borrar

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ var map = new mapboxgl.Map({
     center: [2.290078, 48.895353], // starting position [lng, lat]
     zoom: 17 // starting zoom
 });
+map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+}));
 map.addControl(new mapboxgl.NavigationControl());
-
-
 map.addControl(
     new mapboxgl.GeolocateControl({
         positionOptions: {
@@ -16,18 +17,15 @@ map.addControl(
         trackUserLocation: true
     })
 );
-
 map.on('load', function() {
-
     var layers = map.getStyle().layers;
-
     var labelLayerId;
+
     for (var i = 0; i < layers.length; i++) {
         if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
             labelLayerId = layers[i].id;
             break;
         }
-
     }
     map.addLayer({
         'id': 'porHacer',
@@ -37,7 +35,6 @@ map.on('load', function() {
         'minzoom': 15,
         'paint': {
             'fill-color': ["case", ["boolean", ["feature-state", "seleccionado"], false], "red", "rgba(0,0,0,0)"]
-
         }
     }, labelLayerId);
 
@@ -49,47 +46,67 @@ map.on('load', function() {
         }
     }), labelLayerId;
 
-    map.setFeatureState({
-        source: 'composite',
-        sourceLayer: "building",
-        id: 64799733,
-
-    }, {
-        seleccionado: true
-    });
-    map.setFeatureState({
-        source: 'composite',
-        sourceLayer: "building",
-        id: 64800214,
-
-    }, {
-        seleccionado: true
-    });
-
-    // map.setFeatureState({ id: 64799733, source: "edificiosHechos" });
-
-
-
 })
+var database = firebase.database();
 
-map.on('click', 'porHacer', function(e) {
+database.ref("LevalloisN1")
+    .on("child_added", function(s) {
 
+        map.setFeatureState({
+            source: 'composite',
+            sourceLayer: "building",
+            id: s.val().id,
+
+        }, { seleccionado: true });
+
+    })
+
+database.ref("LevalloisN1")
+    .on("child_removed", function(s) {
+
+        map.setFeatureState({
+            source: 'composite',
+            sourceLayer: "building",
+            id: s.val().id,
+
+        }, { seleccionado: false });
+
+    })
+
+
+map.on('click', function(e) {
+
+    let popup = new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML("<button id ='enviar'onclick='enviarBaseDatos();'>Enviar</button><br><button id ='borrar'onclick='borrarBaseDatos();'>Borrar</button>")
+        .addTo(map);
+
+    function fechaHecho() {
+        var today = new Date()
+        var d = today.getDate();
+        var m = today.getMonth();
+        var y = today.getFullYear();
+        return d + " /" + m + "/ " + y;
+    };
+    var fechaHecho = fechaHecho();
     var features = map.queryRenderedFeatures(e.point);
     var numero = features[0].id;
-
-    map.setFeatureState({
-        source: 'composite',
-        sourceLayer: "building",
+    var edificio = {
         id: numero,
+        fecha: fechaHecho,
+    }
+    document.getElementById('enviar').onclick =
+        function enviarBaseDatos() {
 
-    }, {
-        seleccionado: true
-    });
+            database.ref("LevalloisN1/" + edificio.id)
+                .set(edificio);
+            popup.remove()
+        }
+    document.getElementById('borrar').onclick =
+        function borrarBaseDatos() {
 
-
-    map.getSource('edificiosHechos').setData({
-        "type": "FeatureCollection",
-        "features": e.features,
-    });
+            firebase.database().ref("LevalloisN1/" + numero).remove();
+            popup.remove()
+        };
 
 })
